e26d9aed22678985321e2a3f5509640bc4e14a49
fotimaab <145212829+fotimaab@users.noreply.github.com>
2025-02-15 20:06:19 +0000

    changed

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -3,12 +3,15 @@ package com.task02;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.model.RetentionSetting;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,30 +23,33 @@ import java.util.Map;
 	aliasName = "${lambdas_alias_name}",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-public class HelloWorld implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse>  {
+@LambdaUrlConfig(
+        authType = AuthType.NONE,
+        invokeMode = InvokeMode.BUFFERED
+)
 
-	public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
+public class HelloWorld implements RequestHandler<APIGatewayV2HTTPEvent, Map<String, Object>> {
 
-        String httpMethod = event.getRequestContext().getHttp().getMethod();
-        String path = event.getRequestContext().getHttp().getPath();
+    public Map<String, Object> handleRequest(APIGatewayV2HTTPEvent request, Context context) {
+        Map<String, Object> response = new HashMap<>();
+        String method = getMethod(request);
+        String path = getPath(request);
 
-        // Check if the request is for the /hello GET endpoint
-        if ("/hello".equals(path) && "GET".equalsIgnoreCase(httpMethod)) {
-            return createResponse(200, "{\"message\": \"Hello from Lambda!\"}");
+        if ("/hello".equals(path) && "GET".equals(method)) {
+            response.put("statusCode", 200);
+            response.put("message", "Hello from Lambda");
+        } else {
+            response.put("statusCode", 400);
+            response.put("message", "Bad request syntax or unsupported method. Request path: " + path +". HTTP method: " + method);
         }
-
-        // For all other endpoints or methods, return a 400 Bad Request error
-        String errorMessage = String.format("Bad Request. Unsupported endpoint: %s or method: %s", path, httpMethod);
-        return createResponse(400, "{\"message\": \"" + errorMessage + "\"}");
-	}
-
-	private APIGatewayV2HTTPResponse createResponse(int statusCode, String body) {
-        APIGatewayV2HTTPResponse response = new APIGatewayV2HTTPResponse();
-        response.setStatusCode(statusCode);
-        response.setBody(body);
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Content-Type", "application/json");
-        response.setHeaders(headers);
         return response;
+    }
+
+    private String getMethod(APIGatewayV2HTTPEvent requestEvent) {
+        return requestEvent.getRequestContext().getHttp().getMethod();
+    }
+
+    private String getPath(APIGatewayV2HTTPEvent requestEvent) {
+        return requestEvent.getRequestContext().getHttp().getPath();
     }
 }
